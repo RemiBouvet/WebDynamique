8f9faefe170f879754d89d3843c9eee5c2dfa535
Remi Bouvet <Remi.Bouvet.Etu@univ-lemans.fr>
2015-11-24 12:53:40 +0100

    include

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	include("include/connectionBDD.php");
+	include("include/function.php");
+?>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Page d'acceuil/title>
+	</head>
+	<body>
+		<h1>Page d'acceuil</h1>
+			<?php
+				include("include/authentification.php");
+			?>
+	</body>
+</html>
